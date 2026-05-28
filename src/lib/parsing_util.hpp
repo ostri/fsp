9b3767956085ed350b570eb2a02cecbea587cfeb
Matjaž <matjaz.ostroversnik@gmail.com>
2026-05-28 13:38:20 +0200

    compile time part for xpaths prepared

--- src/lib/parsing_util.hpp
+++ src/lib/parsing_util.hpp
@@ -14,6 +14,7 @@
 namespace fsp
 {
   using cstr_t = std::string_view;
+  // --- compile time exception ----------------------------------------------------------
   class compile_error : public std::exception
   {
   public:
@@ -25,19 +26,21 @@ namespace fsp
   private:
     const char* message;
   };
-
+  // --- prefix / uri definition ---------------------------------------------------------
   struct ns
   {
     cstr_t prefix;
     cstr_t uri;
   };
-
+  // --- xpath element -------------------------------------------------------------------
   struct xpath_el
   {
     cstr_t ns;
     cstr_t tag;
   };
-
+  ////////////////////////////////////////////////////////////////////////////////////////
+  // --- raw xpath definition (name, xpath (attr/array), optional) -----------------------
+  // public attributes and no contructor to be able to make a constant definition in code
   struct raw_attr
   {
   public:
@@ -56,7 +59,9 @@ namespace fsp
     bool   is_opt = false; // is the value optional?
     // NOLINTEND(misc-non-private-member-variables-in-classes)
   };
-  /////////////////////////////////////////////////////////////////////////////
+  ///////////////////////////////////////////////////////////////////////////////////////
+  // --- setof raw_attr elements
+  // dimension calculated in compile time -> std::array
   template <std::size_t N>
   struct raw_inputs_container : std::array<raw_attr, N>
   {
@@ -83,70 +88,19 @@ namespace fsp
     }
   };
 
-  // Dedukcijski vodnik, ki pravilno izpelje velikost 'N' iz std::array
+  // deduction guide to get the dimension from the structure not from explicit value
   template <std::size_t N>
   raw_inputs_container(std::array<fsp::raw_attr, N>) -> raw_inputs_container<N>;
   //////////////////////////////////////////////////////////////////////////////////
 
-  // Pomožna funkcija za odstranjevanje presledkov (White Spaces) in poševnic
-  constexpr cstr_t trim_xpath(cstr_t str)
-  {
-    auto is_ws_or_slash = [](char c) { return c == ' ' || c == '\t' || c == '\n' || c == '\r' || c == '/'; };
 
-    // Poišči začetek brez WS in /
-    const auto* start = std::ranges::find_if_not(str, is_ws_or_slash);
-    if (start == str.end()) return {};
-
-    // Poišči konec brez WS in /
-    const auto* end = std::ranges::find_if_not(str | std::views::reverse, is_ws_or_slash).base();
-
-    return {start, end};
-  }
   consteval cstr_t uri_from_prefix(cstr_t prefix, std::span<const ns> ns_arr)
   {
     for (const auto& el : ns_arr)
-    {
       if (el.prefix == prefix) return el.uri;
-    }
     throw compile_error("Prefix has no matching definition in ns structure.");
   }
-  consteval std::vector<xpath_el> parse_xpath_to_elements(cstr_t input, std::span<const ns> ns_arr)
-  {
-    // 1. Odstrani vodilne in končne presledke ter poševnice
-    cstr_t trimmed = trim_xpath(input);
-    if (trimmed.empty()) return {};
 
-    std::vector<xpath_el> result;
-
-    // 2. C++20/C++23 views za razbijanje niza z delimiterjem '/'
-    // views::split ustvari pod-razpone (subranges) posameznih segmentov
-    for (auto segment_range : trimmed | std::views::split('/'))
-    {
-      // Pretvorba trenutnega segmenta nazaj v string_view (C++23 konstrukcija)
-      cstr_t segment{segment_range};
-
-      if (segment.empty()) continue;
-
-      xpath_el element{};
-
-      // 3. Razbijanje posameznega segmenta glede na dvopičje ':'
-      auto colon = segment.find(':');
-      if (colon != cstr_t::npos)
-      {
-        element.ns  = uri_from_prefix(segment.substr(0, colon), ns_arr);
-        element.tag = segment.substr(colon + 1);
-      }
-      else
-      {
-        element.ns  = uri_from_prefix("", ns_arr);
-        element.tag = segment;
-      }
-
-      result.push_back(element);
-    }
-
-    return result;
-  }
   template <std::size_t xpath_len>
   struct xml_attr
   {
@@ -154,92 +108,156 @@ namespace fsp
     constexpr xml_attr() = default;
     constexpr xml_attr(raw_attr raw, const auto& ns_arr)
     : name_(raw.name)
-    , path_(raw.path)
+    , path_(trim_xpath(raw.path)) // remove trailing & leading whitespaces and '/'
     , is_opt_(raw.is_opt)
     {
-      auto tmp = parse_xpath_to_elements(raw.path, ns_arr); // NOLINT(cppcoreguidelines-prefer-member-initializer)
-      for (std::size_t cnt = 0; cnt < tmp.size(); cnt++) xpath_[cnt] = tmp[cnt];
-      if (xpath_size() > 0)
+      auto tmp    = parse_xpath_to_elements(raw.path, ns_arr); // NOLINT(cppcoreguidelines-prefer-member-initializer)
+      xpath_size_ = tmp.size();
+      for (auto cnt = 0U; cnt < xpath_size_; cnt++)
       {
-        auto& last_el = xpath_.at(xpath_size() - 1);
-        if (last_el.tag.starts_with('@')) is_attr_ = true;              // is attribute ?
-        else if (last_el.tag.starts_with('*')) is_array_ = true;        // has array of values?
-        if (is_attr_ || is_array_) last_el.tag = last_el.tag.substr(1); // strip the marker for attributes and arrays
+        if (tmp[cnt].tag.starts_with('@'))
+        {
+          xpath_size_--;                                                      // attribute is removed from the xpath
+          attr_ = xpath_el{.ns = tmp[cnt].ns, .tag = tmp[cnt].tag.substr(1)}; // strip leading character (@)
+          continue;
+        }
+        if (tmp[cnt].tag.starts_with('*'))
+        {
+          is_array_   = true;
+          xpath_[cnt] = xpath_el{.ns = tmp[cnt].ns, .tag = tmp[cnt].tag.substr(1)};
+          continue;
+        }
+        xpath_[cnt] = tmp[cnt];
       }
+    }
+    static consteval std::vector<xpath_el> parse_xpath_to_elements(cstr_t input, std::span<const ns> ns_arr)
+    {
+      // cstr_t trimmed = trim_xpath(input); // trim leading and trailing whitespaces and /
+      // if (trimmed.empty()) return {};     // no xpath no work
+      if (input.empty()) throw compile_error(fmt::format("empty xpath").data());
+      std::vector<xpath_el> result;
+      for (auto segment_range : input | std::views::split('/')) // split by /
+      {
+        cstr_t segment{segment_range};
+        if (segment.empty()) continue; // if xpath contains multiple // in sequence
+        xpath_el element{};
+        auto     colon = segment.find(':'); // split by colon
+        if (colon != cstr_t::npos)
+        { // ns + tag -> just split and calculate uri
+          element.ns  = uri_from_prefix(segment.substr(0, colon), ns_arr);
+          element.tag = segment.substr(colon + 1);
+        }
+        else
+        { // no prefix: attribue does not inherit default namespace, tag does -> remove prefix
+          element.ns  = segment.starts_with('@') ? "" : uri_from_prefix("", ns_arr);
+          element.tag = segment;
+        }
+        result.push_back(element);
+      }
+      return result;
+    }
+    // Pomožna funkcija za odstranjevanje presledkov (White Spaces) in poševnic
+    static constexpr cstr_t trim_xpath(cstr_t str)
+    {
+      auto        is_ws_or_slash = [](char c) { return c == ' ' || c == '\t' || c == '\n' || c == '\r' || c == '/'; };
+      const auto* start          = std::ranges::find_if_not(str, is_ws_or_slash);
+      if (start == str.end())
+        throw compile_error(fmt::format("Empty xpath after trimming (first). '{}'", str).data()); // empty xpath after left trimming
+      const auto* end = std::ranges::find_if_not(str | std::views::reverse, is_ws_or_slash).base();
+      return {start, end};
+    }
+    [[nodiscard]] constexpr std::string full_xpath() const
+    {
+      std::string tmp;
+      for (auto cnt = 0U; cnt < xpath_size_; cnt++) tmp += fmt::format("[{}]", xpath_[cnt].tag);
+      if (is_attr()) tmp += fmt::format("[{}]@", attr_name());
+      else if (is_array()) tmp += fmt::format("*");
+      return tmp;
+    }
+    [[nodiscard]] constexpr std::string full_xpath_with_uri() const
+    {
+      std::string tmp;
+      for (auto cnt = 0U; cnt < xpath_size_; cnt++) tmp += fmt::format("[{}:{}]", xpath_[cnt].ns, xpath_[cnt].tag);
+      if (is_attr()) tmp += fmt::format("[{}:{}]@", attr_uri(), attr_name());
+      else if (is_array()) tmp += fmt::format("*");
+      return tmp;
     }
     [[nodiscard]] constexpr cstr_t      name() const { return name_; }
     [[nodiscard]] constexpr cstr_t      path() const { return path_; }
     [[nodiscard]] constexpr bool        is_opt() const { return is_opt_; }
-    [[nodiscard]] constexpr bool        is_attr() const { return is_attr_; }
+    [[nodiscard]] constexpr bool        is_attr() const { return ! attr_.tag.empty(); }
     [[nodiscard]] constexpr bool        is_array() const { return is_array_; }
     [[nodiscard]] constexpr auto        xpath() const { return xpath_; }
     [[nodiscard]] constexpr auto&       xpath() { return xpath_; }
-    [[nodiscard]] constexpr std::size_t xpath_size() const { return raw_attr::xpath_size(path_); }
+    [[nodiscard]] constexpr std::size_t xpath_size() const { return xpath_size_; }
     [[nodiscard]] constexpr xpath_el&   last() { return xpath_.at(xpath_size() - 1); }
+    [[nodiscard]] constexpr cstr_t      attr_name() const { return attr_.tag; }
+    [[nodiscard]] constexpr cstr_t      attr_uri() const { return attr_.ns; }
   private:
     cstr_t                          name_;
     cstr_t                          path_;
-    bool                            is_attr_  = false;
     bool                            is_array_ = false;
     bool                            is_opt_   = false;
     std::array<xpath_el, xpath_len> xpath_{};
+    xpath_el                        attr_;           // if it is attribute path it holds a name of the attribute
+    std::size_t                     xpath_size_ = 0; // size of the xpath (number of nonempty elements)
   };
 
   using date_t = std::chrono::year_month_day;
 
-  consteval std::size_t get_xpaths_max_depth(std::span<const raw_attr> attrs)
-  {
-    std::size_t max_d = 0;
-    for (const auto& attr : attrs) { max_d = std::max(max_d, attr.xpath_size()); }
-    return max_d;
-  }
+  // consteval std::size_t get_xpaths_max_depth(std::span<const raw_attr> attrs)
+  // {
+  //   std::size_t max_d = 0;
+  //   for (const auto& attr : attrs) { max_d = std::max(max_d, attr.xpath_size()); }
+  //   return max_d;
+  // }
 
-  class path_node
-  {
-  public:
-    static constexpr const auto max_int = std::numeric_limits<std::size_t>::max();
-    constexpr path_node()
-    : fsp::path_node("", "")
-    {
-    }
-    constexpr path_node(cstr_t uri, cstr_t tag, std::size_t xp_node, std::size_t next_xpath)
-    : uri_(uri)
-    , tag_(tag)
-    , xp_node_(xp_node)
-    , next_xpath_(next_xpath)
-    {
-    }
-    constexpr path_node(cstr_t uri, cstr_t tag)
-    : path_node(uri, tag, max_int, max_int)
-    {
-    }
-    [[nodiscard]] constexpr std::size_t xp_node() const { return xp_node_; }
-    [[nodiscard]] constexpr std::size_t next_xpath() const { return next_xpath_; }
-    [[nodiscard]] constexpr bool        is_xpath_last_node() const { return xp_node_ == max_int; }
-  private:
-    cstr_t      uri_;                  // tag's uri
-    cstr_t      tag_;                  // tag name
-    std::size_t xp_node_    = max_int; // xpath node or max_int if this is the end of the xpath
-    std::size_t next_xpath_ = max_int; // next xpath
-  };
+  // class path_node
+  // {
+  // public:
+  //   static constexpr const auto max_int = std::numeric_limits<std::size_t>::max();
+  //   constexpr path_node()
+  //   : fsp::path_node("", "")
+  //   {
+  //   }
+  //   constexpr path_node(cstr_t uri, cstr_t tag, std::size_t xp_node, std::size_t next_xpath)
+  //   : uri_(uri)
+  //   , tag_(tag)
+  //   , xp_node_(xp_node)
+  //   , next_xpath_(next_xpath)
+  //   {
+  //   }
+  //   constexpr path_node(cstr_t uri, cstr_t tag)
+  //   : path_node(uri, tag, max_int, max_int)
+  //   {
+  //   }
+  //   [[nodiscard]] constexpr std::size_t xp_node() const { return xp_node_; }
+  //   [[nodiscard]] constexpr std::size_t next_xpath() const { return next_xpath_; }
+  //   [[nodiscard]] constexpr bool        is_xpath_last_node() const { return xp_node_ == max_int; }
+  // private:
+  //   cstr_t      uri_;                  // tag's uri
+  //   cstr_t      tag_;                  // tag name
+  //   std::size_t xp_node_    = max_int; // xpath node or max_int if this is the end of the xpath
+  //   std::size_t next_xpath_ = max_int; // next xpath
+  // };
   // template </*std::size_t path_node_size,*/ std::size_t paths_size, std::size_t xpath_size>
   template <const auto& ra>
   class path_node_struct
   {
   public:
-    static constexpr const auto xpath_size          = ra.get_max_xpath_len();
-    static constexpr const auto paths_size          = ra.size();
+    static constexpr const auto MAX_XPATH_SIZE      = ra.get_max_xpath_len();
+    static constexpr const auto PATHS_SIZE          = ra.size();
     static constexpr const auto path_node_deck_size = ra.get_total_xpath_len();
     consteval path_node_struct(std::span<const raw_attr> inputs, std::span<const ns> ns_arr)
     {
       for (std::size_t i = 0; i < inputs.size(); ++i)
       {
-        auto tmp = xml_attr<xpath_size>(inputs[i], ns_arr);
-        data[i]  = tmp;
+        auto tmp      = xml_attr<MAX_XPATH_SIZE>(inputs[i], ns_arr);
+        xpath_dscr[i] = tmp;
         // push_xpath(tmp.xpath().begin(), deck, 0);
       }
     }
-    consteval std::size_t get_new_node() { return first_free_deck_++; }
+    //    consteval std::size_t get_new_node() { return first_free_deck_++; }
     // constexpr std::size_t push_xpath(const auto& xpath_el, auto& deck, std::size_t deck_pos)
     // {
     //   if ((xpath_el.uri() == deck[deck_pos].uri()) && (xpath_el.tag() == deck[deck_pos].tag()))
@@ -247,26 +265,76 @@ namespace fsp
     //     /// xpath and deck element are equal
     //   }
     // }
-    constexpr xml_attr<xpath_size> operator[](std::size_t ndx) const { return data[ndx]; }
-    constexpr xml_attr<xpath_size> operator[](const cstr_t ndx) const
+    constexpr xml_attr<MAX_XPATH_SIZE> operator[](std::size_t ndx) const { return xpath_dscr[ndx]; }
+    constexpr xml_attr<MAX_XPATH_SIZE> operator[](const cstr_t ndx) const
     {
-      for (const auto& el : data)
+      for (const auto& el : xpath_dscr)
         if (el.name() == ndx) return el;
       throw compile_error(fmt::format("unknown path '{}'.", ndx).data());
     }
-    constexpr auto                      begin() const { return data.begin(); }
-    constexpr auto                      end() const { return data.end(); }
-    [[nodiscard]] constexpr std::size_t size() const { return paths_size; }
+    constexpr auto                      begin() const { return xpath_dscr.begin(); }
+    constexpr auto                      end() const { return xpath_dscr.end(); }
+    [[nodiscard]] constexpr std::size_t size() const { return PATHS_SIZE; }
+    [[nodiscard]] constexpr std::size_t max_xpath_size() const { return MAX_XPATH_SIZE; }
+
+    /*!
+     * The method returns the value of the xpath tag, that has the lowest value in the list of
+     * provided xpaths on the provided depth
+     * E.g.
+     * a/b/c
+     * a/b/d
+     * c/d/e/f
+     * first_xpath_tag_name(0) -> a
+     * first_xpath_tag_name(1) -> b
+     * first_xpath_tag_name(2) -> c
+     * first_xpath_tag_name(3) -> f
+     * first_xpath_tag_name(4) -> empty
+     */
+    [[nodiscard]] constexpr cstr_t first_xpath_tag_name(std::size_t depth) const
+    {
+      static_assert(MAX_XPATH_SIZE != 0, "No xpath definitions. There must be at leas one xpath definition provided.");
+      if (depth >= MAX_XPATH_SIZE) //
+        throw compile_error(fmt::format("too deep. depth:{} max depth: {}", depth, MAX_XPATH_SIZE).data());
+      cstr_t res;
+      for (const auto& el : xpath_dscr) //
+      {
+        auto val = el.xpath()[depth].tag;
+        if (res.empty() && ! val.empty())
+        {
+          res = val; // we found a nonempty tag in xpath on the provided depth
+          continue;
+        };
+        if (! val.empty() && val < res) res = val; // we found smaller
+      }
+      return res;
+    }
+    [[nodiscard]] constexpr cstr_t last_xpath_tag_name(std::size_t depth) const
+    {
+      static_assert(MAX_XPATH_SIZE != 0, "No xpath definitions. There must be at leas one xpath definition provided.");
+      if (depth >= MAX_XPATH_SIZE) //
+        throw compile_error(fmt::format("too deep. depth:{} max depth: {}", depth, MAX_XPATH_SIZE).data());
+      cstr_t res;
+      for (const auto& el : xpath_dscr) //
+      {
+        auto val = el.xpath()[depth].tag;
+        if (res.empty() && ! val.empty())
+        {
+          res = val; // we found a nonempty tag in xpath on the provided depth
+          continue;
+        };
+        if (! val.empty() && val > res) res = val; // we found bigger (testing empty is redundant)
+      }
+      return res;
+    }
   private:
-    std::array<xml_attr<xpath_size>, paths_size> data;
-    std::array<path_node, path_node_deck_size>   deck{};
-    std::size_t                                  first_free_deck_ = 0; // index of the first free deck element
+    std::array<xml_attr<MAX_XPATH_SIZE>, PATHS_SIZE> xpath_dscr;
+    //    std::array<path_node, path_node_deck_size>   deck{};
+    // std::size_t first_free_deck_ = 0; // index of the first free deck element
     //    std::array<path_node, path_node_size>        nodes;
   };
   //....................................................................................
   template <const auto& raw_paths, const auto& ns_arr>
   consteval auto build()
-  //  { return path_node_struct<raw_paths.size(), raw_paths.get_max_xpath_len()>(raw_paths, ns_arr); }
   { return path_node_struct<raw_paths>(raw_paths, ns_arr); }
 
 } // namespace fsp
--- src/test/test_pars.cpp
+++ src/test/test_pars.cpp
@@ -22,73 +22,88 @@ namespace fsp
     {.name="remmitance_",     .path="CdtTrfTxInf/RmtInf/Strd/RfrdDocInf/*Nb", .is_opt=true},
   });
   // clang-format on
-  constexpr auto tree_node = build<raw, NS>();
+  constexpr auto xtn = build<raw, NS>(); // xml tree node(s)
 
   // NOLINTBEGIN(readability-magic-numbers)
   // Testi zastavic v času prevajanja
-  static_assert(tree_node["txn_id"].name() == "txn_id");
-  static_assert(! tree_node["txn_id"].is_attr());
-  static_assert(! tree_node["txn_id"].is_opt());
+  static_assert(xtn["txn_id"].name() == "txn_id");
+  static_assert(! xtn["txn_id"].is_attr());
+  static_assert(! xtn["txn_id"].is_opt());
 
   // Test za "currency_", ki ima is_attr = true in is_opt = true
-  static_assert(tree_node["currency_"].name() == "currency_");
-  static_assert(tree_node["currency_"].is_attr());
-  static_assert(tree_node["currency_"].is_opt());
-  static_assert(tree_node["currency_"].last().tag == "Ccy");
+  static_assert(xtn["currency_"].name() == "currency_");
+  static_assert(xtn["currency_"].is_attr());
+  static_assert(xtn["currency_"].is_opt());
+  static_assert(xtn["currency_"].attr_name() == "Ccy");
 
   // Test za "value_date_", ki ima is_opt = true
-  static_assert(! tree_node["value_date_"].is_attr());
-  static_assert(tree_node["value_date_"].is_opt());
+  static_assert(! xtn["value_date_"].is_attr());
+  static_assert(xtn["value_date_"].is_opt());
   // Test za "remittance_", ki ima is_attr = true in is_opt = true
-  static_assert(tree_node["remmitance_"].name() == "remmitance_");
-  static_assert(! tree_node["remmitance_"].is_attr());
-  static_assert(tree_node["remmitance_"].is_opt());
-  static_assert(tree_node["remmitance_"].last().tag == "Nb");
-  // NOLINTEND(readability-magic-numbers)
+  static_assert(xtn["remmitance_"].name() == "remmitance_");
+  static_assert(! xtn["remmitance_"].is_attr());
+  static_assert(xtn["remmitance_"].is_opt());
+  static_assert(xtn["remmitance_"].last().tag == "Nb");
+  //  NOLINTEND(readability-magic-numbers)
 }; // namespace fsp
 
 int main()
 {
   try
   {
+    constexpr const auto line_len = 120U;
     fmt::print("\n");
     for (const auto& el : fsp::NS) { fmt::print("prefix:'{:10}' uri: '{}'\n", el.prefix, el.uri); }
-    fmt::print("\nNamespace(s):");
+    fmt::print("\nNamespace(s):\n");
     // Izpis tabele z vsemi atributi za kontrolo med izvajanjem
-    fmt::print("Calculated array depth: {}\n", fsp::get_xpaths_max_depth(fsp::raw));
+    //    fmt::print("Calculated xpath depth: {}\n", fsp::get_xpaths_max_depth(fsp::raw));
+    fmt::print("Calculated xpath depth: {}\n", fsp::xtn.max_xpath_size());
     fmt::print("{:<15} | {:<40} | {:<7} | {:<8} | {:<7} | {}\n", "Name", "Path", "Is_Attr", "Is_Array", "Is_Opt", "XPath");
-    fmt::print("{:-<90}\n", "");
+    fmt::print("{:-<{}}\n", "", line_len);
 
-    for (const auto& attr : fsp::tree_node)
+    for (const auto& attr : fsp::xtn)
     {
-      std::string xpath_str;
-      for (size_t i = 0; i < attr.xpath_size(); ++i)
-      {
-        xpath_str += fmt::format("[{}]", //
-                                         //  attr.xpath.at(i).ns.empty() ? "def" : std::string(attr.xpath.at(i).ns) + "×",
-                                         //  attr.xpath.at(i).tag);
-                                 attr.xpath().at(i).tag);
-      }
-
+      std::string xpath_str = attr.full_xpath();
       fmt::print("{:<15} | {:<40} | {:<7} | {:<8} | {:<7} | {}\n",
                  attr.name(),
                  attr.path(),
-                 attr.is_attr(),
-                 attr.is_array(),
-                 attr.is_opt(),
+                 attr.is_attr() ? "yes" : " ",
+                 attr.is_array() ? "yes" : " ",
+                 attr.is_opt() ? "yes" : " ",
                  xpath_str);
     }
 
-    // fmt::print("currency: '{}'\n", fsp::tree_node["currency_"].xpath()[0].tag);
-    // fmt::print("currency: '{}'\n", fsp::tree_node["currency_"].xpath()[1].tag);
-    // fmt::print("currency: '{}'\n", fsp::tree_node["currency_"].xpath()[2].tag);
-    // fmt::print("currency: '{}'\n", fsp::tree_node["currency_"].last().tag);
-    // fmt::print("remmitance: '{}'\n", fsp::tree_node["remmitance_"].xpath()[0].tag);
-    // fmt::print("remmitance: '{}'\n", fsp::tree_node["remmitance_"].xpath()[1].tag);
-    // fmt::print("remmitance: '{}'\n", fsp::tree_node["remmitance_"].xpath()[2].tag);
-    // fmt::print("remmitance: '{}'\n", fsp::tree_node["remmitance_"].xpath()[3].tag);
-    // fmt::print("remmitance: '{}'\n", fsp::tree_node["remmitance_"].xpath()[4].tag);
-    // fmt::print("remmitance: '{}'\n", fsp::tree_node["remmitance_"].last().tag);
+    fmt::print("currency: '{}'\n", fsp::xtn["currency_"].xpath()[0].tag);
+    fmt::print("currency: '{}'\n", fsp::xtn["currency_"].xpath()[1].tag);
+    fmt::print("currency: '{}'\n", fsp::xtn["currency_"].xpath()[2].tag);
+    fmt::print("currency: '{}'\n", fsp::xtn["currency_"].last().tag);
+    fmt::print("currency: '{}'\n", fsp::xtn["currency_"].attr_name());
+    fmt::print("currency: '{}'\n", fsp::xtn["currency_"].full_xpath());
+    fmt::print("currency: '{}'\n", fsp::xtn["currency_"].full_xpath_with_uri());
+    fmt::print("remmitance: '{}'\n", fsp::xtn["remmitance_"].xpath()[0].tag);
+    fmt::print("remmitance: '{}'\n", fsp::xtn["remmitance_"].xpath()[1].tag);
+    fmt::print("remmitance: '{}'\n", fsp::xtn["remmitance_"].xpath()[2].tag);
+    fmt::print("remmitance: '{}'\n", fsp::xtn["remmitance_"].xpath()[3].tag);
+    fmt::print("remmitance: '{}'\n", fsp::xtn["remmitance_"].xpath()[4].tag);
+    fmt::print("remmitance: '{}'\n", fsp::xtn["remmitance_"].last().tag);
+    fmt::print("remmitance: '{}'\n", fsp::xtn["remmitance_"].attr_name());
+    fmt::print("remmitance: '{}'\n", fsp::xtn["remmitance_"].full_xpath());
+    fmt::print("remmitance: '{}'\n", fsp::xtn["remmitance_"].full_xpath_with_uri());
+    fmt::print("lowest and the biggest tag name value by depth\n");
+    for (auto ndx = 0U; ndx < fsp::xtn.max_xpath_size(); ndx++) //
+    {
+      fmt::print("depth: {:3}: min: {:15} max: {:15}\n", +ndx, fsp::xtn.first_xpath_tag_name(ndx), fsp::xtn.last_xpath_tag_name(ndx));
+    }
+    static_assert(fsp::xtn.first_xpath_tag_name(0) == "CdtTrfTxInf");
+    static_assert(fsp::xtn.last_xpath_tag_name(0) == "CdtTrfTxInf");
+    static_assert(fsp::xtn.first_xpath_tag_name(1) == "CdtrAcct");
+    static_assert(fsp::xtn.last_xpath_tag_name(1) == "RmtInf");
+    static_assert(fsp::xtn.first_xpath_tag_name(2) == "FinInstnId");
+    static_assert(fsp::xtn.last_xpath_tag_name(2) == "TxId");
+    static_assert(fsp::xtn.first_xpath_tag_name(3) == "BICFI");
+    static_assert(fsp::xtn.last_xpath_tag_name(3) == "RfrdDocInf");
+    static_assert(fsp::xtn.first_xpath_tag_name(4) == "Nb");
+    static_assert(fsp::xtn.last_xpath_tag_name(4) == "Nb");
   }
   catch (...)
   {
