edf3ab131ec8a703447bedd4803368f2a2ca90d4
Matjaž <matjaz.ostroversnik@gmail.com>
2026-07-10 10:26:32 +0200

    is_release() & is_debug()

--- src/lib/common.hpp
+++ src/lib/common.hpp
@@ -11,13 +11,10 @@ namespace fsp
   using cstr_t       = std::string_view;
   // Whitespace characters to trim
   constexpr cstr_t WHITESPACE = " \t\n\r";
-
   // Trims whitespace from the start of the string_view
   [[nodiscard]] constexpr cstr_t ltrim(cstr_t str, cstr_t ws = WHITESPACE) noexcept;
-
   // Trims whitespace from the end of the string_view
   [[nodiscard]] constexpr cstr_t rtrim(cstr_t str, cstr_t ws = WHITESPACE) noexcept;
-
   // Trims whitespace from both ends of the string_view
   [[nodiscard]] constexpr cstr_t trim(cstr_t str, cstr_t ws = WHITESPACE) noexcept;
 
@@ -36,5 +33,13 @@ namespace fsp
   }
   constexpr cstr_t trim(cstr_t str, cstr_t ws) noexcept { return rtrim(ltrim(str, ws), ws); }
 
-
+  consteval bool is_release()
+  {
+#ifdef NDEBUG
+    return true;
+#else
+    return false;
+#endif
+  }
+  consteval bool is_debug() { return ! is_release(); }
 } // namespace fsp
