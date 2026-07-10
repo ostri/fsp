e42f61c8c90e590f19a650e2c9143f57e09a6562
Matjaž <matjaz.ostroversnik@gmail.com>
2026-07-10 09:36:58 +0200

    strict_validation removed

--- src/lib/processor_config.hpp
+++ src/lib/processor_config.hpp
@@ -7,27 +7,27 @@ namespace fsp
   // Configuration for the processor
   struct processor_config
   {
-    proc_data targets;                     // NOLINT(misc-non-private-member-variables-in-classes)
-    size_t    num_workers          = 0;    // NOLINT(misc-non-private-member-variables-in-classes)
-    bool      validate_against_xsd = true; // NOLINT(misc-non-private-member-variables-in-classes)
-    bool      strict_validation    = true; // NOLINT(misc-non-private-member-variables-in-classes)
-    //    std::optional<std::string> schema_namespace;
-    logger_config             log_config; // NOLINT(misc-non-private-member-variables-in-classes)
-    [[nodiscard]] std::string dump(int offs) const
-    {
-      std::string msg;
-      msg = fmt::format(R"({0}targets:{1}
-{0}num workers:{2}
-{0}validate:{3}
-{0}strict validation:{4}
-{0}logger: {5})",
-                        std::string(offs, ' '),
-                        targets.dump(offs),
-                        num_workers,
-                        validate_against_xsd,
-                        strict_validation,
-                        log_config.logger_name);
-      return msg;
-    }
+    // NOLINTBEGIN(misc-non-private-member-variables-in-classes)
+    proc_data     targets;                     // target points to split the xml document
+    size_t        num_workers          = 0;    // number of workers processing one file
+    bool          validate_against_xsd = true; // should we validate with xsd
+    logger_config log_config;                  // configuration of the
+    // NOLINTEND(misc-non-private-member-variables-in-classes)
+    [[nodiscard]] std::string dump(int offs) const;
   };
+
+  inline std::string processor_config::dump(int offs) const
+  {
+    std::string msg;
+    msg = fmt::format(R"({0}targets:{1}
+  {0}num workers:{2}
+  {0}validate:{3}
+  {0}logger: {4})",
+                      std::string(offs, ' '),
+                      targets.dump(offs),
+                      num_workers,
+                      validate_against_xsd,
+                      log_config.logger_name);
+    return msg;
+  }
 } // namespace fsp
