8e3e18b848c0cf5f29b87fca9eb5c9c81a7988da
Matjaž <matjaz.ostroversnik@gmail.com>
2026-07-15 15:51:27 +0200

    profiling without validation

---
+++ shell/profile_no_xsd.sh
@@ -0,0 +1,12 @@
+#/bin/bash
+set -x
+folder=profile_$$
+mkdir $folder
+cd $folder
+cmake ../.. -DCMAKE_BUILD_TYPE=Profile
+cmake --build . --parallel --target pacs8
+x=../../xml-data/pacs8-1M.xml
+y=../../xsd/pacs.008.xsd
+perf record -F 999 -g -- ./pacs8 $x $x $x $x $x $x $x $x $x $x
+perf report > report.txt
+perf report
