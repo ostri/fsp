c4de22b5cd3cacc3a5534436992a1590a5685527
Matjaž <matjaz.ostroversnik@gmail.com>
2026-07-23 19:19:12 +0200

    bugfix

--- shell/bench10M_no_xsd.sh
+++ shell/bench10M_no_xsd.sh
@@ -1,6 +1,6 @@
 #/bin/bash
 set -x
-cmake ../.. -DCMAKE_BUILD_TYPE=Release
+cmake .. -DCMAKE_BUILD_TYPE=Release
 cmake --build . --parallel --target pacs8
 x=../xml-data/pacs8-1M.xml
 y=../xsd/pacs.008.xsd
