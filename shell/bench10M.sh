0b019f2b4c4d47844ec44a18a126f9b169ed27a9
Matjaž <matjaz.ostroversnik@gmail.com>
2026-07-27 11:19:25 +0200

    less verbose output

--- shell/bench10M.sh
+++ shell/bench10M.sh
@@ -4,5 +4,5 @@ cmake ../.. -DCMAKE_BUILD_TYPE=Release
 cmake --build . --parallel --target pacs8
 x=../xml-data/pacs8-1M.xml
 y=../xsd/pacs.008.xsd
-clear && /usr/bin/time -v ./pacs8 $x $x $x $x $x $x $x $x $x $x $y
+clear && time ./pacs8 $x $x $x $x $x $x $x $x $x $x $y
 
