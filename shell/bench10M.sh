3ef980297359cf1d099a0562d93fae716b6bd805
Matjaž <matjaz.ostroversnik@gmail.com>
2026-08-22 21:57:26 +0200

    adaptation to changed build location

--- shell/bench10M.sh
+++ shell/bench10M.sh
@@ -2,7 +2,7 @@
 set -x
 cmake .. -DCMAKE_BUILD_TYPE=Release
 cmake --build . --parallel --target pacs8
-x=../xml-data/pacs8-1M.xml
-y=../xsd/pacs.008.xsd
+x=~/fsp/xml-data/pacs8-1M.xml
+y=~/fsp/xsd/pacs.008.xsd
 clear && time ./pacs8 $x $x $x $x $x $x $x $x $x $x $y
 
